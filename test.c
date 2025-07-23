e358a43b738f363d59906c20d32238fdf2eed7a2
Benjamin Stürz <benni@stuerz.xyz>
2025-07-23 21:18:19 +0000

    test: write socket demo

--- test.c
+++ test.c
@@ -1,6 +1,61 @@
+#include <netinet/in.h>
 #include <unistd.h>
 #include <stdio.h>
 #include "sys/task.h"
+
+void client_task (void *arg)
+{
+	int clientfd = (int)(size_t)arg;
+	char buf[64];
+	int i, n, x;
+
+	task_printf ("accepted client: %d\n", clientfd);
+	while (1) {
+		n = task_read (clientfd, buf, sizeof (buf));
+		if (n == 0)
+			break;
+		if (n < 0)
+			task_err (1, "read()");
+
+		
+		for (i = 0; i < n; i += x) {
+			x = task_write (clientfd, buf + i, n - i);
+			if (x <= 0)
+				task_err (1, "write()");
+		}
+	}
+	close (clientfd);
+	task_exit (0);
+}
+
+void server_task (void *arg)
+{
+	struct sockaddr_in addr;
+	int sockfd, clientfd, optval = 1;
+
+	sockfd = socket (AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		task_err (1, "socket()");
+
+	setsockopt (sockfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof (optval));
+
+	addr.sin_len = sizeof (addr);
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons (8000);
+	addr.sin_addr.s_addr = INADDR_ANY;
+	if (bind (sockfd, (struct sockaddr *)&addr, sizeof (addr)) != 0)
+		task_err (1, "bind()");
+
+	if (listen (sockfd, 0) != 0)
+		task_err (1, "listen()");
+
+	while (1) {
+		clientfd = task_accept (sockfd, NULL, NULL);
+		if (clientfd < 0)
+			task_err (1, "accept()");
+		task_spawn ("client", client_task, (void *)(size_t)clientfd);
+	}
+}
 
 void loop_task (void *arg)
 {
@@ -46,6 +101,7 @@ void other_task (void *arg)
 	}
 
 	task_spawn ("io", io_task, NULL);
+	task_spawn ("server", server_task, NULL);
 
 	task_exit (42);
 }
